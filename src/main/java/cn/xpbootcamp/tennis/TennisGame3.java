41037c4dc2b36afa806abb0b2ea67171a8031ca4
Qiqi Zhao <qiqzhao@thoughtworks.com>
2020-03-05 13:02:12 +0800

    simplify condition

--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -6,6 +6,7 @@ public class TennisGame3 implements TennisGame {
     private int p1Point;
     private String p1Name;
     private String p2Name;
+    private final String[] points = new String[]{Constant.LOVE, Constant.FIFTEEN, Constant.THIRTY, Constant.FORTY};
 
     public TennisGame3(String p1N, String p2N) {
         this.p1Name = p1N;
@@ -13,23 +14,22 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String s;
-        if (p1Point < 4 && p2Point < 4 && !(p1Point + p2Point == 6)) {
-            String[] p = new String[]{Constant.LOVE, Constant.FIFTEEN, Constant.THIRTY, Constant.FORTY};
-            s = p[p1Point];
-            return (p1Point == p2Point) ? s + Constant.HYPHEN + Constant.ALL : s + Constant.HYPHEN + p[p2Point];
-        } else {
-            if (p1Point == p2Point)
-                return Constant.DEUCE;
-            s = p1Point > p2Point ? p1Name : p2Name;
-            return ((p1Point - p2Point)*(p1Point - p2Point) == 1) ? Constant.ADVANTAGE + Constant.SPACE + s : Constant.WIN_FOR + Constant.SPACE+ s;
+        String score;
+        if (p1Point < 4 && p2Point < 4 && p1Point + p2Point != 6) {
+            score = points[p1Point] + Constant.HYPHEN;
+            return (p1Point == p2Point) ? score + Constant.ALL : score + points[p2Point];
         }
+        if (p1Point == p2Point) {
+            return Constant.DEUCE;
+        }
+        score = Constant.SPACE + (p1Point > p2Point ? p1Name : p2Name);
+        return ((p1Point - p2Point) * (p1Point - p2Point) == 1) ? Constant.ADVANTAGE + score : Constant.WIN_FOR + score;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if ("player1".equals(playerName)) {
             this.p1Point += 1;
-        else
+        } else
             this.p2Point += 1;
 
     }
