5103b30881a9782939207dfa9ebbd96565402273
Qiqi Zhao <qiqzhao@thoughtworks.com>
2020-03-05 12:55:52 +0800

    rename game3

--- src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
+++ src/main/java/cn/xpbootcamp/tennis/TennisGame3.java
@@ -2,35 +2,35 @@ package cn.xpbootcamp.tennis;
 
 public class TennisGame3 implements TennisGame {
 
-    private int p2;
-    private int p1;
-    private String p1N;
-    private String p2N;
+    private int p2Point;
+    private int p1Point;
+    private String p1Name;
+    private String p2Name;
 
     public TennisGame3(String p1N, String p2N) {
-        this.p1N = p1N;
-        this.p2N = p2N;
+        this.p1Name = p1N;
+        this.p2Name = p2N;
     }
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (p1Point < 4 && p2Point < 4 && !(p1Point + p2Point == 6)) {
             String[] p = new String[]{Constant.LOVE, Constant.FIFTEEN, Constant.THIRTY, Constant.FORTY};
-            s = p[p1];
-            return (p1 == p2) ? s + Constant.HYPHEN + Constant.ALL : s + Constant.HYPHEN + p[p2];
+            s = p[p1Point];
+            return (p1Point == p2Point) ? s + Constant.HYPHEN + Constant.ALL : s + Constant.HYPHEN + p[p2Point];
         } else {
-            if (p1 == p2)
+            if (p1Point == p2Point)
                 return Constant.DEUCE;
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? Constant.ADVANTAGE + Constant.SPACE + s : Constant.WIN_FOR + Constant.SPACE+ s;
+            s = p1Point > p2Point ? p1Name : p2Name;
+            return ((p1Point - p2Point)*(p1Point - p2Point) == 1) ? Constant.ADVANTAGE + Constant.SPACE + s : Constant.WIN_FOR + Constant.SPACE+ s;
         }
     }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.p1Point += 1;
         else
-            this.p2 += 1;
+            this.p2Point += 1;
 
     }
 
